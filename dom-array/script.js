a6ced744b37baa9e56e5091631b0e64aad3ca1e5
connorjnel <connor.j.nel00@gmail.com>
2023-02-08 15:55:39 +0000

    dom update and map

--- dom-array/script.js
+++ dom-array/script.js
@@ -16,5 +16,51 @@ getRandomUser();
 async function getRandomUser() {
 	const res = await fetch('https://randomuser.me/api');
 	const data = await res.json();
-	console.log(data);
+
+	const user = data.results[0];
+
+	const newUser = {
+		name: `${user.name.first} ${user.name.last}`,
+		money: Math.floor(Math.random() * 1000000),
+	};
+
+	addData(newUser);
 }
+
+// Double money
+function doubleMoney() {
+	data = data.map((user) => {
+		return { ...user, money: user.money * 2 };
+	});
+
+	updateDOM();
+}
+
+// Add new object to data array
+function addData(obj) {
+	data.push(obj);
+
+	updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+	// Clear Main Div
+	main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+	providedData.forEach((item) => {
+		const element = document.createElement('div');
+		element.classList.add('person');
+		element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+		main.appendChild(element);
+	});
+}
+
+// Format number as money
+function formatMoney(number) {
+	return '£ ' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event Listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
