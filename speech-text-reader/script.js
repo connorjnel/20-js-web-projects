55d178bf45f7ed6ceb9474519a881b9b6c97fce1
connorjnel <connor.j.nel00@gmail.com>
2023-03-10 20:36:49 +0000

    speech added

--- speech-text-reader/script.js
+++ speech-text-reader/script.js
@@ -70,10 +70,20 @@ function createBox(item) {
     <p class="info">${text}</p>  
     `;
 
-	//TODO Add speak event
+	box.addEventListener('click', () => {
+		setTextMessage(text);
+		speakText();
+
+		// Add active effect
+		box.classList.add('active');
+		setTimeout(() => box.classList.remove('active'), 800);
+	});
 
 	main.appendChild(box);
 }
+
+// Init speech synth
+const message = new SpeechSynthesisUtterance();
 
 // Store voices
 let voices = [];
@@ -90,6 +100,16 @@ function getVoices() {
 
 		voicesSelect.appendChild(option);
 	});
+}
+
+// Set text
+function setTextMessage(text) {
+	message.text = text;
+}
+
+// Speak Text
+function speakText() {
+	speechSynthesis.speak(message);
 }
 
 // Voices changed
